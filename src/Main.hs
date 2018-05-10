92c77f5eeef191491753a81425ffc839383f89a3
hallavad <hallavad@outlook.com>
2018-05-10 19:02:31 +0200

    saker

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,3 @@
-
-
 module Main where
 
 import qualified Data.ByteString.Lazy as B
@@ -7,7 +5,7 @@ import           Network.HTTP
 import           Network.HTTP.Listen
 
 listener :: Listener B.ByteString IO
-listener request = print request >> return Nothing
+listener request@Request{rqBody=a} = print request >> print a >> return Nothing
 
 main :: IO ()
 main = print "Funkar nu" >> run 6660 listener
